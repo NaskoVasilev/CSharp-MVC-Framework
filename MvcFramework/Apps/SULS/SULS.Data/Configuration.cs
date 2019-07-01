79e5428052eb1c1cbc61565f02e1ea84dc6bfbfe
NaskoVasilev <nasko_vasilev@abv.bg>
2019-07-01 23:23:47 +0300

    Add dbContext class and configure connection to the database

---
+++ MvcFramework/Apps/SULS/SULS.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace SULS.Data
+{
+	public class Configuration
+	{
+		public static string ConnectionString = @"Server=DESKTOP-07K6OOE\SQLEXPRESS;Database=SulsApp;Trusted_Connection=True;";
+	}
+}
---
+++ MvcFramework/Apps/SULS/SULS.Data/SULSContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SULS.Models;
+
+namespace SULS.Data
+{
+    public class SULSContext : DbContext
+    {
+		public DbSet<User> Users { get; set; }
+
+		public DbSet<Submission> Submissions { get; set; }
+
+		public DbSet<Problem> Problems { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if(!optionsBuilder.IsConfigured)
+			{
+				optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+			}
+
+			base.OnConfiguring(optionsBuilder);
+		}
+	}
+}
